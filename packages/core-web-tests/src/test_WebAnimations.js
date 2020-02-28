62dc0ad0e1a7cb7903b572f23c329ebd40b1d6e0
Simon Menke <simon.menke@gmail.com>
2020-02-28 22:10:05 +0000

    More tests

---
+++ packages/core-web-tests/src/test_WebAnimations.js
@@ -0,0 +1,22 @@
+QUnit.test("WebAnimations", assert => {
+  const el = document.createElement("div");
+  document.body.appendChild(el);
+  assert.equal(window.getComputedStyle(el).opacity, "1");
+
+  const animations = el.animate(
+    {
+      opacity: [1, 0.5]
+    },
+    {
+      duration: 250,
+      fill: "forwards"
+    }
+  );
+
+  const done = assert.async();
+  animations.onfinish = () => {
+    done();
+    assert.equal(window.getComputedStyle(el).opacity, "0.5");
+    el.remove();
+  };
+});
---
+++ packages/core-web-tests/src/test_requestIdleCallback.js
@@ -0,0 +1,7 @@
+QUnit.test("requestIdleCallback", assert => {
+  const done = assert.async();
+  requestIdleCallback(() => {
+    assert.ok(true, "expected requestIdleCallback to call");
+    done();
+  });
+});
