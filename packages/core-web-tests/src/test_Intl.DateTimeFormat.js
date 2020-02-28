e0636597dcf7f64e1089143d19ba46f4e1623822
Simon Menke <simon.menke@gmail.com>
2020-02-28 16:50:50 +0000

    Date is local to the servers

--- packages/core-web-tests/src/test_Intl.DateTimeFormat.js
+++ packages/core-web-tests/src/test_Intl.DateTimeFormat.js
@@ -1,5 +1,5 @@
 QUnit.test("Intl.DateTimeFormat ", function(assert) {
-  const date = new Date(Date.UTC(2012, 11, 20, 3, 0, 0));
+  const date = new Date(Date.UTC(2012, 11, 20, 12, 0, 0));
   const remove_LEFT_TO_RIGHT_MARKER = s => s.replace(/\u200E/g, "");
   assert.equal(
     remove_LEFT_TO_RIGHT_MARKER(new Intl.DateTimeFormat("en-US").format(date)),
