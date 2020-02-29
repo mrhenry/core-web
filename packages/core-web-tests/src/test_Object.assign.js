9e99bdd736e3714b15e3900900fb30d453a145cc
romainmenke <romainmenke@gmail.com>
2020-02-29 12:01:07 +0100

    test : Object.assign

---
+++ packages/core-web-tests/src/test_Object.assign.js
@@ -0,0 +1,19 @@
+QUnit.test("Element.prototype.classList", function(assert) {
+	const fixture = document.getElementById('qunit-fixture');
+
+	const div = document.createElement('div');
+	fixture.appendChild(div);
+
+	Object.assign(div, {
+		width: '100px',
+		height: '200px',
+	});
+	assert.equal( div.width, '100px' );
+	assert.equal( div.height, '200px' );
+
+	Object.assign(div.style, {
+		backgroundColor: 'black',
+	});
+
+	assert.equal( div.getAttribute('style'), 'background-color: black;' );
+});
