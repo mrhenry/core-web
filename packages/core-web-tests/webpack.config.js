1c8dc5e78b077e67a9fdc81fe1a678e6e0ef1dc4
Simon Menke <simon.menke@gmail.com>
2020-03-02 10:16:39 +0000

    mjs is also js

--- packages/core-web-tests/webpack.config.js
+++ packages/core-web-tests/webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
 	module: {
 		rules: [
 			{
-				test: /\.js$/,
+				test: /\.m?js$/,
 				include: /core-web\/modules|abort-controller/,
 				use: {
 					loader: "babel-loader",
