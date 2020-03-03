9c343cd6f4b0262487f1599b1cec1d3c8a2131c5
romainmenke <romainmenke@gmail.com>
2020-03-03 11:25:10 +0100

    check-browser cli

---
+++ packages/core-web-check-browser/README.md
@@ -0,0 +1,5 @@
+# `core-web-check-browser`
+
+`npx @mrhenry/core-web-check-browser --browser=samsung --browser-version=40`
+
+`npx @mrhenry/core-web-check-browser --browser=samsung_mob --browser-version=40`
---
+++ packages/core-web-check-browser/index.js
@@ -0,0 +1,59 @@
+#!/usr/bin/env node
+
+const args = process.argv.slice(2);
+const pkg = require('./package.json');
+const UA = require('@financial-times/polyfill-useragent-normaliser');
+
+const USAGE = 'Usage:\n' +
+	'  core-web-check-browser\n' +
+	'  core-web-check-browser --browser=chrome --browser-version=79\n';
+
+function isArg (arg) {
+		return args.some(function (str) {
+		return str === arg || str.indexOf(arg + '=') === 0;
+	})
+}
+
+function error (msg) {
+	process.stderr.write('core-web-check-browser: ' + msg + '\n');
+	process.exit(1);
+}
+
+if (isArg('--help') || isArg('-h')) {
+	process.stdout.write(pkg.description + '.\n\n' + USAGE + '\n')
+} else if (isArg('--version') || isArg('-v')) {
+	process.stdout.write(pkg.name + ' ' + pkg.version + '\n')
+} else {
+	let browser = '';
+	let version = '0';
+
+	for (var i = 0; i < args.length; i++) {
+		var arg = args[i].split('=')
+		var name = arg[0]
+		var value = arg[1]
+
+		if (value) value = value.replace(/^["']|["']$/g, '')
+
+		if (name === '--browser' || name === '-b') {
+			browser = value
+		} else if (name === '--browser-version' || name === '-r') {
+			version = value
+		} else {
+			error('Unknown arguments ' + args[i] + '.\n\n' + USAGE)
+		}
+	}
+
+	const ua = new UA(`${browser}/${version}`);
+	if (ua.isUnknown()) {
+		process.stdout.write('unknown browser : ' + browser + ' ' + version + '\n');
+		process.stdout.write('!!! all detected features will be polyfilled !!!\n');
+		process.stdout.write('main browser names are :\n');
+
+		Object.keys(UA.getBaselines()).forEach((key) => {
+			process.stdout.write('  - ' + key + '\n');
+		});
+
+	} else {
+		process.stdout.write('browser family  : ' + ua.getFamily() + '\nbrowser version : ' + ua.getVersion() + '\n');
+	}
+}
