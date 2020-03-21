6df0bd3cbd66aa28b286997ff2d23ae5015e5f1e
romainmenke <romainmenke@gmail.com>
2020-03-21 11:59:10 +0100

    custom elements : attachShadow test

---
+++ packages/core-web-tests/src/test_customElements.attachShadow.js
@@ -0,0 +1,28 @@
+QUnit.test('customElements.attachShadow', function(assert) {
+	class TestElementShadow extends HTMLElement {
+		constructor() {
+			// Always call super first in constructor
+			super();
+
+			// Create a shadow root
+			var shadow = this.attachShadow({mode: 'open'});
+
+			// Create text node and add word count to it
+			var spacer = document.createElement('div');
+			spacer.setAttribute('style', 'width: 100px; height: 20px;');
+			this.setAttribute('style', 'display: inline-block;');
+
+			// Append it to the shadow root
+			shadow.appendChild(spacer);
+		}
+	}
+
+	customElements.define('test-element-shadow', TestElementShadow);
+	const fixture = document.getElementById('qunit-fixture');
+
+	fixture.innerHTML = '<test-element-shadow></test-element-shadow>';
+
+	const el = fixture.querySelector('test-element-shadow');
+	assert.equal(window.getComputedStyle(el).width, '100px');
+	assert.equal(window.getComputedStyle(el).height, '20px');
+});
