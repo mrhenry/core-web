53582c2a4cd8f3eb0155c1748b362ded6ad9527f
romainmenke <romainmenke@gmail.com>
2020-03-21 11:11:14 +0100

    try ios13

--- packages/core-web-tests/src/test_AbortController.js
+++ packages/core-web-tests/src/test_AbortController.js
@@ -3,11 +3,6 @@ QUnit.test('AbortController', async assert => {
 	const ac = new AbortController();
 
 	await fetch('package.json', { signal: ac.signal });
-
 	ac.abort();
-
-	// Ensure we call "fetch" in a later event loop cycle.
-	setTimeout(() => {
-		assert.rejects(fetch('package.json', { signal: ac.signal }));
-	}, 0);
+	assert.rejects(fetch('package.json', { signal: ac.signal }));
 });
