3dd4aa6d71857f840205956644c26c176c6701d9
romainmenke <romainmenke@gmail.com>
2020-03-21 10:01:20 +0100

    custom elements : test ios safari

--- packages/core-web-tests/src/test_customElements.define.js
+++ packages/core-web-tests/src/test_customElements.define.js
@@ -1,15 +1,38 @@
 QUnit.test('customElements.define', function(assert) {
-	class TestElement extends HTMLElement {
+	class TestElementA extends HTMLElement {
 		foo() {
 			return 'baz';
 		}
 	}
 
-	customElements.define('test-element', TestElement);
+	customElements.define('test-element-a', TestElementA);
 	const fixture = document.getElementById('qunit-fixture');
 
-	fixture.innerHTML = '<test-element></test-element>';
+	fixture.innerHTML = '<test-element-a></test-element-a>';
 
-	const el = fixture.querySelector('test-element');
+	const el = fixture.querySelector('test-element-a');
 	assert.equal(el.foo(), 'baz');
+});
+
+QUnit.test('customElements.connectedCallback', function(assert) {
+	class TestElementB extends HTMLElement {
+		constructor() {
+			super();
+
+			this._content = 'rendered content';
+		}
+
+		connectedCallback() {
+			this.innerHTML = this._content || '';
+		}
+	}
+
+	customElements.define('test-element-b', TestElementB);
+	const fixture = document.getElementById('qunit-fixture');
+	const elStart = new TestElementB();
+
+	fixture.appendChild(elStart);
+
+	const elOut = fixture.querySelector('test-element-b');
+	assert.equal(elOut.innerHTML, 'rendered content');
 });
