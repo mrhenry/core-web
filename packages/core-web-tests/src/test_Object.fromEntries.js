3cfd9ad487176a7a099577802e6b4787b2c17fec
romainmenke <romainmenke@gmail.com>
2020-03-28 19:09:00 +0100

    object fromEntries : test ie11

--- packages/core-web-tests/src/test_Object.fromEntries.js
+++ packages/core-web-tests/src/test_Object.fromEntries.js
@@ -1,4 +1,4 @@
-QUnit.test('Object.assign', function(assert) {
+QUnit.test('Object.fromEntries', function(assert) {
 	// https://github.com/Financial-Times/polyfill-library/issues/552
 	Object.fromEntries([["a", 1], ["b", 2]]);
 	assert.ok(true, 'it worked');
