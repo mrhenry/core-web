deb06cdf300a3ae7bd60d3c3d5c298b30205d3b5
Romain Menke <11521496+romainmenke@users.noreply.github.com>
2020-04-20 20:21:34 +0200

    test match media on ios 10 (#88)
    * test match media on ios 10
    
    * test match media on ios 10
    
    * add matchMedia addListener test
    
    * remove ios test run again

--- packages/core-web-tests/src/test_matchMedia.js
+++ packages/core-web-tests/src/test_matchMedia.js
@@ -1,4 +1,10 @@
 QUnit.test('matchMedia', assert => {
 	const mql = window.matchMedia('(max-width: 200px)');
 	assert.equal(mql.matches, false);
+
+	mql.addListener(() => {
+		console.log('listened');
+	});
+
+	assert.ok(true, 'it worked');
 });
