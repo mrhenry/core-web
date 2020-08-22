30531be8f62ac2d21e2b64cf696e2af4fd9eeb1b
romainmenke <romainmenke@gmail.com>
2020-08-22 10:59:55 +0200

    fix it ?

--- packages/core-web-tests/src/test_Intl.RelativeTimeFormat.js
+++ packages/core-web-tests/src/test_Intl.RelativeTimeFormat.js
@@ -1,6 +1,5 @@
-import '@mrhenry/core-web/modules/Intl.~locale.en';
-// import "@mrhenry/core-web/modules/Intl.PluralRules.~locale.en";
-// import "@mrhenry/core-web/modules/Intl.RelativeTimeFormat.~locale.en";
+import '@mrhenry/core-web/modules/Intl.PluralRules.~locale.en';
+import '@mrhenry/core-web/modules/Intl.RelativeTimeFormat.~locale.en';
 
 QUnit.test('Intl.RelativeTimeFormat', function(assert) {
 	const format = new Intl.RelativeTimeFormat('en', { style: 'narrow' });
