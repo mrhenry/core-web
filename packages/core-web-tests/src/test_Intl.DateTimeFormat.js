fa3f01f04e23abfd50fa47caab3c8477dec7a3bf
Romain Menke <11521496+romainmenke@users.noreply.github.com>
2020-10-29 19:16:30 +0100

    update intl tests (#176)
    * update intl tests
    
    * expect Intl to have no interop issues

--- packages/core-web-tests/src/test_Intl.DateTimeFormat.js
+++ packages/core-web-tests/src/test_Intl.DateTimeFormat.js
@@ -1,15 +1,29 @@
-import "@mrhenry/core-web/modules/Intl.~locale.en-US";
-import "@mrhenry/core-web/modules/Intl.~locale.en-GB";
+import '@mrhenry/core-web/modules/Intl.DateTimeFormat.~locale.en-GB';
+import '@mrhenry/core-web/modules/Intl.DateTimeFormat.~locale.en-US-POSIX';
 
-QUnit.test("Intl.DateTimeFormat ", function(assert) {
+QUnit.test('Intl.DateTimeFormat – en', function (assert) {
 	const date = new Date(Date.UTC(2012, 11, 20, 12, 0, 0));
-	const remove_LEFT_TO_RIGHT_MARKER = s => s.replace(/\u200E/g, "");
+	const remove_LEFT_TO_RIGHT_MARKER = s => s.replace(/\u200E/g, '');
 	assert.equal(
-		remove_LEFT_TO_RIGHT_MARKER(new Intl.DateTimeFormat("en-US").format(date)),
-		"12/20/2012"
+		remove_LEFT_TO_RIGHT_MARKER(new Intl.DateTimeFormat('en').format(date)),
+		'12/20/2012'
 	);
+});
+
+QUnit.test('Intl.DateTimeFormat – en-GB', function (assert) {
+	const date = new Date(Date.UTC(2012, 11, 20, 12, 0, 0));
+	const remove_LEFT_TO_RIGHT_MARKER = s => s.replace(/\u200E/g, '');
 	assert.equal(
-		remove_LEFT_TO_RIGHT_MARKER(new Intl.DateTimeFormat("en-GB").format(date)),
-		"20/12/2012"
+		remove_LEFT_TO_RIGHT_MARKER(new Intl.DateTimeFormat('en-GB').format(date)),
+		'20/12/2012'
+	);
+});
+
+QUnit.test('Intl.DateTimeFormat – en-US-POSIX', function (assert) {
+	const date = new Date(Date.UTC(2012, 11, 20, 12, 0, 0));
+	const remove_LEFT_TO_RIGHT_MARKER = s => s.replace(/\u200E/g, '');
+	assert.equal(
+		remove_LEFT_TO_RIGHT_MARKER(new Intl.DateTimeFormat('en-US-POSIX').format(date)),
+		'12/20/2012'
 	);
 });
