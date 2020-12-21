eaa5c7000f004ea6144aaccb3c7470bc41937eb5
romainmenke <romainmenke@gmail.com>
2020-12-21 17:38:49 +0100

    gh pages : basic webpack config

---
+++ packages/pages/webpack.config.js
@@ -0,0 +1,127 @@
+const path = require('path');
+
+module.exports = [
+	configBuilder({
+		name: '2020',
+		target: [
+			'web',
+			'browserslist:Chrome 87 or Edge 87 or Firefox 83 or Opera 72 or Safari 14 or Samsung 13 or Android 81'
+		],
+		coreJsBrowsers: [
+			'chrome >= 87',
+			'edge >= 87',
+			'firefox >= 83',
+			'opera >= 72',
+			'safari >= 14',
+			'samsung >= 13.0',
+			'android >= 81',
+		],
+		coreWebBrowsers: {
+			chrome: '87',
+			edge: '87',
+			firefox: '83',
+			opera: '72',
+			safari: '14',
+			samsung_mob: '13.0',
+			// android: '81',
+		},
+	}),
+
+	configBuilder({
+		name: '2014',
+		target: [
+			'web',
+			'browserslist:Chrome 39 or Firefox 83 or Opera 26 or Safari 8 or Samsung 13 or Android 4.4.4'
+		],
+		coreJsBrowsers: [
+			'chrome >= 39',
+			'firefox >= 34',
+			'opera >= 26',
+			'safari >= 8',
+			'android >= 4.4.4',
+		],
+		coreWebBrowsers: {
+			chrome: '39',
+			firefox: '34',
+			opera: '26',
+			safari: '8',
+			android: '4.4.4',
+		},
+	}),
+];
+
+function configBuilder(opts) {
+	return {
+		mode: 'production',
+		context: path.resolve(__dirname, 'lib'),
+		output: {
+			path: path.resolve(__dirname, 'dist'),
+			filename: `[name].${opts.name}.js`
+		},
+		entry: {
+			'index': './js/index.js'
+		},
+		devtool: 'source-map',
+		target: opts.target,
+		optimization: {
+			minimize: true
+		},
+		module: {
+			rules: [
+				{
+					test: /\.js$/,
+					include: /(core-web\/modules|core-web\/helpers)/,
+					use: {
+						loader: 'babel-loader',
+						options: {
+							comments: false,
+							presets: [
+								[
+									'@babel/preset-env',
+									{
+										corejs: '^3.6.3',
+										targets: {
+											browsers: opts.coreJsBrowsers
+										},
+										useBuiltIns: 'usage'
+									}
+								]
+							]
+						}
+					}
+				},
+				{
+					test: /\.js$/,
+					exclude: /(node_modules|core-web\/modules|core-web\/helpers)/,
+					use: {
+						loader: 'babel-loader',
+						options: {
+							comments: false,
+							plugins: [
+								[
+									'@mrhenry/core-web',
+									{
+										browsers: opts.coreWebBrowsers,
+										debug: true
+									}
+								]
+							],
+							presets: [
+								[
+									'@babel/preset-env',
+									{
+										corejs: '^3.6.3',
+										targets: {
+											browsers: opts.coreJsBrowsers
+										},
+										useBuiltIns: 'usage'
+									}
+								]
+							]
+						}
+					}
+				}
+			]
+		}
+	};
+}
