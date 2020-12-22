43ab0e8fc3b78f99303b26b2ca86ea144b0b6573
Thomas Verleye <thomasverleye@live.be>
2020-12-22 14:24:57 +0100

    Hero: html and basic styling

--- packages/pages/lib/landing/landing.js
+++ packages/pages/lib/landing/landing.js
@@ -5,6 +5,7 @@ const path = require('path');
 const html = require('../html/html');
 
 const siteHeader = require('./templates/site-header');
+const hero = require('./templates/hero');
 
 main();
 
@@ -50,6 +51,12 @@ function generate() {
 	])}
 
 	<main id="main-content">
+		<div class="section">
+			<div class="wrapper">
+				${hero()}
+			</div>
+		</div>
+		
 		<div class="section">
 			<div class="wrapper">
 				<h1>HELLO WORLD 💰</h1>
