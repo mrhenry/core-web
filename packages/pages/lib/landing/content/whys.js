c02e30a36044ae9d880013d674d20d03131c033a
Romain Menke <11521496+romainmenke@users.noreply.github.com>
2020-12-22 16:01:16 +0100

    pages : content (#328)
    * pages : content
    
    * Update packages/pages/lib/landing/content/whys.js
    
    Co-authored-by: Simon Menke <simon.menke@gmail.com>
    
    * tweak
    
    * more content
    
    * wording
    
    * Update packages/pages/lib/landing/content/whys.js
    
    Co-authored-by: Simon Menke <simon.menke@gmail.com>
    
    Co-authored-by: Simon Menke <simon.menke@gmail.com>

---
+++ packages/pages/lib/landing/content/whys.js
@@ -0,0 +1,41 @@
+const polyfillIoLink = require("../snippets/polyfill-io-link");
+const html = require('../../html/html');
+
+module.exports = {
+	items: [
+		{
+			title: 'One less blocking http request',
+			content: html`
+Most visitors use a modern browser which doesn't need any polyfills.
+When polyfills are bundled there is no need for an extra http request.
+For most visitors your pages will render faster.
+`.trim()
+		},
+		{
+			title: 'One less point of failure',
+			content: html`
+Any external client side dependency has some inherent risk.
+Converting this to a build depedency reduces this.
+`.trim()
+		},
+		{
+			title: 'Reproducible builds',
+			content: html`
+When an external polyfill provider changes or updates there code they potentially introduce bugs impacting your code. Packaging polyfills with your code prevents this from hapening.
+`.trim()
+		},
+		{
+			title: 'Sourcemaps',
+			content: html`
+Because your build process sees every polyfill you can have sourcemaps.
+This makes it easier to report and debug issues.
+`.trim()
+		},
+		{
+			title: 'Control over updates',
+			content: html`
+You can add your safeguards and only update core-web when all your requirements are met.
+`.trim()
+		}
+	]
+}
