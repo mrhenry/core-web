98be97e5ee93887197a9bee263e58785ad28c898
romainmenke <romainmenke@gmail.com>
2020-12-22 23:02:10 +0100

    more html

--- packages/pages/lib/landing/templates/roadmap.js
+++ packages/pages/lib/landing/templates/roadmap.js
@@ -2,20 +2,20 @@ const html = require('../../html/html');
 
 function renderMilestones(milestones) {
 	let hasCurrent = false;
-	let hasFuture = false;
+	// let hasFuture = false;
 	let output = "";
 	milestones.forEach((milestone, index) => {
 		if ('current' === milestone.type && !hasCurrent) {
 			hasCurrent = true;
-			output += '<h4 class="roadmap__future">Where we are</h4>';
+			output += html`<h4 class="roadmap__future">Where we are</h4>`;
 		}
 
 		// if ('future' === milestone.type && !hasFuture) {
 		// 	hasFuture = true;
-		// 	output += '<h4 class="roadmap__future">Future Milestones</h4>';
+		// 	output += html`<h4 class="roadmap__future">Future Milestones</h4>`;
 		// }
 
-		output += `
+		output += html`
 			<article class="milestone milestone--${milestone.type}" data-emoji="${milestone.emoji}" aria-description="${milestone.type} milestone">
 				<h3 class="milestone__title">${milestone.title}</h3>
 				<div class="milestone__description">${milestone.description}</div>
