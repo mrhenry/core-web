c4ac2bcace508e971326df5ca19e2541b35f345c
romainmenke <romainmenke@gmail.com>
2020-12-22 12:02:02 +0100

    tweaks

--- packages/pages/lib/landing/templates/site-header.js
+++ packages/pages/lib/landing/templates/site-header.js
@@ -1,24 +1,22 @@
 const html = require('../../html/html');
 
 module.exports = function siteHeader(menu) {
-    return html`
-        <div class="section">
-            <div class="wrapper">
-                <div class="site-header">
-                    <a
-                        class="logo"
-                        href="/"
-                    >
-                        core-web
-                    </a>
+	return html`
+		<div class="section">
+			<div class="wrapper">
+				<div class="site-header">
+					<a
+						class="logo"
+						href="/"
+						>core-web</a>
 
-                    <nav class="navigation" aria-label="page sections">
-                        <ul class="navigation__list">
-                            ${menu.map((item) => `<li class="navigation__list-item"><a href="${item.url}" class="navigation__link">${item.title}</a></li>`).reduce((accumulator,item) => accumulator + item)}
-                        </ul>
-                    </nav>
-                </div>
-            </div>
-        </div>
-    `;
+					<nav class="navigation" aria-label="page sections">
+						<ul class="navigation__list">
+							${menu.map((item) => `<li class="navigation__list-item"><a href="${item.url}" class="navigation__link">${item.title}</a></li>`).reduce((accumulator,item) => accumulator + item)}
+						</ul>
+					</nav>
+				</div>
+			</div>
+		</div>
+	`;
 }
