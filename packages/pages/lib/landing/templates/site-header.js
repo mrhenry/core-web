15482cd9d10bd92722fd77ae6971a5a21c570813
Wout Thielemans <wout@mrhenry.be>
2020-12-23 10:39:52 +0100

    Tweak site header style

--- packages/pages/lib/landing/templates/site-header.js
+++ packages/pages/lib/landing/templates/site-header.js
@@ -2,22 +2,20 @@ const html = require('../../html/html');
 
 module.exports = function siteHeader(menu) {
 	return html`
-		<div class="section">
-			<div class="wrapper">
-				<div class="site-header">
-					<a
-						class="logo"
-						href="/"
-					>
-						core-web
-					</a>
+		<div class="wrapper">
+			<div class="site-header">
+				<a
+					class="logo"
+					href="/"
+				>
+					core-web
+				</a>
 
-					<nav class="navigation" aria-label="page sections">
-						<ul class="navigation__list">
-							${menu.map((item) => `<li class="navigation__list-item"><a href="${item.url}" class="navigation__link">${item.title}</a></li>`).join('')}
-						</ul>
-					</nav>
-				</div>
+				<nav class="navigation" aria-label="page sections">
+					<ul class="navigation__list">
+						${menu.map((item) => `<li class="navigation__list-item"><a href="${item.url}" class="navigation__link">${item.title}</a></li>`).join('')}
+					</ul>
+				</nav>
 			</div>
 		</div>
 	`;
