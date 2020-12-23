338cc84768f53c1865549545b2a9098fe2889b60
Thomas Verleye <thomasverleye@live.be>
2020-12-23 14:33:32 +0100

    Roadmap Animation (#333)
    * Roadmap Animation
    
    * tweaks
    
    * add max-width and paragraph line-height to make the design peeps happy
    
    * Butter up the transition a little
    
    * Add a little bit more butter
    
    Co-authored-by: romainmenke <romainmenke@gmail.com>
    Co-authored-by: Wout Thielemans <wout@mrhenry.be>

--- packages/pages/lib/js/index.js
+++ packages/pages/lib/js/index.js
@@ -1,2 +1,34 @@
-console.log('hello!');
-new IntersectionObserver(() => { });
+const milestoneObserver = new IntersectionObserver((entries) => {
+	if (!entries || entries.length < 1) {
+			return;
+	}
+
+	entries.forEach(entry => {
+		if (
+			entry.intersectionRatio > 0.6 &&
+			entry.target &&
+			!entry.target.hasAttribute('milestone-visible')
+		) {
+			entry.target.setAttribute('milestone-visible', 'true');
+		} else {
+				entry.target.removeAttribute('milestone-visible');
+		}
+	});
+}, {
+	threshold: 0.6
+});
+
+function init() {
+	const milestones = document.querySelectorAll('[milestone-observer]');
+	if (!milestones || milestones.length < 1) {
+			return;
+	}
+
+	milestones.forEach((el) => {
+			milestoneObserver.observe(el);
+	});
+};
+
+init();
+requestAnimationFrame(init);
+document.addEventListener("DOMContentLoaded", init);
