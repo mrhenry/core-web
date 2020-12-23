276f67c254926d64f0f9143b9b7d2c6d9f4efb5d
romainmenke <romainmenke@gmail.com>
2020-12-23 13:00:48 +0100

    tweaks

--- packages/pages/lib/js/index.js
+++ packages/pages/lib/js/index.js
@@ -1,33 +1,34 @@
+const milestoneObserver = new IntersectionObserver((entries) => {
+	if (!entries || entries.length < 1) {
+			return;
+	}
 
-let init = function() {
-    const milestones = document.querySelectorAll('[milestone-observer]');
+	entries.forEach(entry => {
+		if (
+			entry.intersectionRatio > 0.6 &&
+			entry.target &&
+			!entry.target.hasAttribute('milestone-visible')
+		) {
+			entry.target.setAttribute('milestone-visible', 'true');
+		} else {
+				entry.target.removeAttribute('milestone-visible');
+		}
+	});
+}, {
+	threshold: 0.6
+});
 
-    if (!milestones || milestones.length < 1) {
-        return;
-    }
+function init() {
+	const milestones = document.querySelectorAll('[milestone-observer]');
+	if (!milestones || milestones.length < 1) {
+			return;
+	}
 
-    init = () => {}; // noop
-
-    var intersectionObserver = new IntersectionObserver((entries) => {
-        if (!entries || entries.length < 1) {
-            return;
-        }
-
-        entries.forEach(entry => {
-            if (
-                entry.isIntersecting &&
-                entry.target &&
-                !entry.target.hasAttribute('milestone-visible')
-            ) {
-                entry.target.setAttribute('milestone-visible', 'true');
-            }
-        });
-    });
-
-    milestones.forEach((el) => {
-        intersectionObserver.observe(el);
-    });
+	milestones.forEach((el) => {
+			milestoneObserver.observe(el);
+	});
 };
 
+init();
 requestAnimationFrame(init);
 document.addEventListener("DOMContentLoaded", init);
