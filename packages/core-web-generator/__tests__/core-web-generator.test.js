d298c74ea62266b35cb66a980a07e9352e230bfb
romainmenke <romainmenke@gmail.com>
2020-12-29 17:26:45 +0100

    more tests

--- packages/core-web-generator/__tests__/core-web-generator.test.js
+++ packages/core-web-generator/__tests__/core-web-generator.test.js
@@ -106,6 +106,55 @@ const browsersToEnginesCases = [
 		},
 		engines: { "WebKit": "85 - 601.5.17" },
 	},
+
+	// From polyfill-library
+
+	{
+		message: "HTMLPictureElement",
+		browsers: {
+			edge: "<13",
+			edge_mob: "<13",
+			ie: "*",
+			ie_mob: "10 - *",
+			firefox: "4 - 37",
+			opera: "11.6 - 29", // incorrect config upstream?
+			safari: "6 - 9",
+			chrome: "* - 38", // incorrect config upstream?
+			ios_saf: "* - 9.2",
+			firefox_mob: "4 - 37",
+			bb: "7 - 10",
+			android: "<52", // incorrect config upstream?
+		},
+		engines: { 
+			Blink: '28 - 38',
+			EdgeHTML: '12 - 12',
+			Gecko: '2 - 37',
+			Presto: '>= 2.10',
+			Trident: '>= 4.0',
+			WebKit: '522.11 - 601.5.17',
+		 },
+	},
+
+	{
+		message: "Symbol.prototype.description",
+		browsers: {
+			edge: "13 - 18",
+			edge_mob: ">= 13",
+			safari: "9 - 12.1",
+			chrome: "49 - 69",
+			firefox: "36 - 62",
+			firefox_mob: "36 - 62",
+			ios_saf: "9 - 12.1",
+			opera: "37 - 56",
+			samsung_mob: "5 - 10.1",
+		},
+		engines: { 
+			Blink: '49 - 69',
+			EdgeHTML: '>= 13',
+			Gecko: '36 - 62',
+			WebKit: '601.1.56 - 607.1.40',
+		 },
+	},
 ];
 
 browsersToEnginesCases.forEach((testcase) => {
