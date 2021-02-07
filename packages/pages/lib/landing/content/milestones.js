4539bf83526e8b41d9d0fc8bc8327b65246028bd
Romain Menke <11521496+romainmenke@users.noreply.github.com>
2021-02-07 15:20:49 +0100

    rebuild (#395)
    * rebuild
    
    * cleanup pages

--- packages/pages/lib/landing/content/milestones.js
+++ packages/pages/lib/landing/content/milestones.js
@@ -10,16 +10,16 @@ module.exports = [
 		emoji: '🐛',
 		type: 'past',
 		title: 'Test extensively',
-		description: html`<p>We have been testing core-web in production.<br>Using our unique insights we have reporting and fixed a large amount of bugs <a href="https://github.com/Financial-Times/polyfill-library/pulls?q=is%3Apr+is%3Amerged+author%3Aromainmenke" target="_blank" rel="noopener">upstream</a>.</p>`
+		description: html`<ul style="margin: 0; padding-left: 1rem;"><li>it's in production</li><li>fixing bugs <a href="https://github.com/Financial-Times/polyfill-library/pulls?q=is%3Apr+is%3Amerged+author%3Aromainmenke" target="_blank" rel="noopener">upstream</a></li>`
 	},
 	{
 		emoji: '🚀',
-		type: 'current',
+		type: 'past',
 		title: '1Y in production',
 	},
 	{
 		emoji: '🔧',
-		type: 'future',
+		type: 'current',
 		title: 'Stable API',
 		description: html`<p>We value your time and want to avoid costly migrations.<br>When we ship v1 we want to make sure it sticks.</p>`
 	},
