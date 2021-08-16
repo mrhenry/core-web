a15b5f6198437c00340aa36030bb78b3db2d2230
Romain Menke <11521496+romainmenke@users.noreply.github.com>
2021-08-16 09:18:39 +0200

    update dependencies (#717)

--- packages/pages/lib/css/to-html.js
+++ packages/pages/lib/css/to-html.js
@@ -19,12 +19,12 @@ module.exports = function cssTags(assetMap, entryPoint) {
 	let cssTags = targets.flatMap((target) => {
 		return assetMap.css[entryPoint][target].manifest.flatMap((manifestEntry) => {
 			const tags = [
-				html`<link rel="stylesheet" media="${manifestEntry.media}" href="/css/${manifestEntry.base}" ua-target="${target}">`
+				html`<link rel="stylesheet" media="${manifestEntry.media || 'all'}" href="/css/${manifestEntry.base}" ua-target="${target}">`
 			];
 
 			if (target === '2011') {
 				tags.push(
-					html`<link rel="stylesheet" media="${manifestEntry.media}" href="/css/${manifestEntry.base}" ua-target="fallback">`
+					html`<link rel="stylesheet" media="${manifestEntry.media || 'all'}" href="/css/${manifestEntry.base}" ua-target="fallback">`
 				);
 			}
 
