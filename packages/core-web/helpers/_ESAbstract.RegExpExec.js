f897433e9c99d77ac2ad6b53d7943319d8df9d44
romainmenke <romainmenke@gmail.com>
2021-11-11 22:25:30 +0100

    update polyfill-library

---
+++ packages/core-web/helpers/_ESAbstract.RegExpExec.js
@@ -0,0 +1,28 @@
+import Call from "@mrhenry/core-web/helpers/_ESAbstract.Call";
+import Get from "@mrhenry/core-web/helpers/_ESAbstract.Get";
+import IsCallable from "@mrhenry/core-web/helpers/_ESAbstract.IsCallable";
+import Type from "@mrhenry/core-web/helpers/_ESAbstract.Type";
+
+// _ESAbstract.RegExpExec
+/* global Call, Get, IsCallable, Type */
+
+// 22.2.5.2.1 RegExpExec ( R, S )
+function RegExpExec(R, S) { // eslint-disable-line no-unused-vars
+	// 1. Let exec be ? Get(R, "exec").
+	var exec = Get(R, 'exec');
+	// 2. If IsCallable(exec) is true, then
+	if (IsCallable(exec)) {
+		// 2.a. Let result be ? Call(exec, R, « S »).
+		var result = Call(exec, R, [ S ]);
+		// 2.b. If Type(result) is neither Object nor Null, throw a TypeError exception.
+		if (Type(result) !== 'object' && Type(result) !== 'null') {
+			throw new TypeError('Invalid result: must be an object or null.');
+		}
+		// 2.c. Return result.
+		return result;
+	}
+	// 3. Perform ? RequireInternalSlot(R, [[RegExpMatcher]]).
+	// 4. Return ? RegExpBuiltinExec(R, S).
+	return Call(RegExp.prototype.exec, R, [ S ]);
+}
+export default RegExpExec;
