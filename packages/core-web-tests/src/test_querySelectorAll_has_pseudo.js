5c9b5e7b5ab3dd80ee8ac844475aa0420a3824cc
romainmenke <romainmenke@gmail.com>
2021-12-28 18:36:56 +0100

    tweak

--- packages/core-web-tests/src/test_querySelectorAll_has_pseudo.js
+++ packages/core-web-tests/src/test_querySelectorAll_has_pseudo.js
@@ -9,13 +9,6 @@ function testSelectorAllFromMain(assert, selector, expected) {
 	assert.deepEqual(formatElements(actual), formatElements(expected));
 }
 
-// Test that |selector| returns the given elements in the given scope element
-function testSelectorAllFromScope(assert, scope, selector, expected) {
-	assert.step(`${selector} matches expected elements from scope ${scope.id || scope.tagName}`);
-	let actual = Array.from(scope.querySelectorAll(selector));
-	assert.deepEqual(formatElements(actual), formatElements(expected));
-}
-
 // Test that |selector| returns the given element in #main.
 function testSelector(assert, selector, expected) {
 	assert.step(`${selector} matches expected element`);
@@ -32,22 +25,6 @@ function testClosest(assert, node, selector, expected) {
 function testMatches(assert, node, selector, expected) {
 	assert.step(`${selector} matches expectedly`);
 	assert.equal(node.matches(selector), expected);
-}
-
-// Test that |selector1| and |selector2| returns same elements in the given scope element
-function compareSelectorAll(assert, scope, selector1, selector2) {
-	let result1 = Array.from(scope.querySelectorAll(selector1));
-	let result2 = Array.from(scope.querySelectorAll(selector2));
-	assert.step(`${selector1} and ${selector2} returns same elements on ${scope.id}`);
-	assert.equal(formatElements(result1), formatElements(result2));
-}
-
-// To test expected diffs from spec/wpt
-function compareSelectorAllNotEqual(assert, scope, selector1, selector2) {
-	let result1 = Array.from(scope.querySelectorAll(selector1));
-	let result2 = Array.from(scope.querySelectorAll(selector2));
-	assert.step(`not : ${selector1} and ${selector2} returns same elements on ${scope.id}`);
-	assert.notEqual(formatElements(result1), formatElements(result2));
 }
 
 var supportsIsQueries = false;
@@ -149,119 +126,6 @@ QUnit.module("querySelector with :has", function () {
 			".sibling:has(.descendant) matches expected element",
 			"closest(.ancestor:has(.descendant)) returns expected element",
 			":has(.target ~ .sibling .descendant) matches expectedly"
-		] );
-	});
-
-	QUnit.test(":has argument with explicit scope (tentative)", function (assert) {
-		const fixture = document.getElementById("qunit-fixture");
-		fixture.innerHTML = `<main>
-			<div id=d01 class="a">
-				<div id=scope1 class="b">
-					<div id=d02 class="c">
-						<div id=d03 class="c">
-							<div id=d04 class="d"></div>
-						</div>
-					</div>
-					<div id=d05 class="e"></div>
-				</div>
-			</div>
-			<div id=d06>
-				<div id=scope2 class="b">
-					<div id=d07 class="c">
-						<div id=d08 class="c">
-							<div id=d09></div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>`;
-
-		const scope1 = document.getElementById("scope1");
-		const scope2 = document.getElementById("scope2");
-		const d02 = document.getElementById("d02");
-		const d03 = document.getElementById("d03");
-		
-		// descendants of a scope element cannot have the scope element as its descendant
-		testSelectorAllFromScope(assert, scope1, ":has(:scope)", []);
-		testSelectorAllFromScope(assert, scope1, ":has(:scope .c)", [d02]);
-		testSelectorAllFromScope(assert, scope1, ":has(.a :scope)", []);
-
-		// there can be more simple and efficient alternative for a ':scope' in ':has'
-		testSelectorAllFromScope(assert, scope1, ".a:has(:scope) .c", []);
-		testSelectorAllFromScope(assert, scope2, ".a:has(:scope) .c", []);
-
-		if (supportsIsQueries) {
-			compareSelectorAllNotEqual(assert, scope1, ".a:has(:scope) .c", ":is(.a :scope .c)");
-			compareSelectorAll(assert, scope2, ".a:has(:scope) .c", ":is(.a :scope .c)");
-			testSelectorAllFromScope(assert, scope1, ".c:has(:is(:scope .d))", [d02, d03]);
-			compareSelectorAll(assert, scope1, ".c:has(:is(:scope .d))", ":scope .c:has(.d)");
-			compareSelectorAll(assert, scope1, ".c:has(:is(:scope .d))", ".c:has(.d)");
-			testSelectorAllFromScope(assert, scope2, ".c:has(:is(:scope .d))", []);
-			compareSelectorAll(assert, scope2, ".c:has(:is(:scope .d))", ":scope .c:has(.d)");
-			compareSelectorAll(assert, scope2, ".c:has(:is(:scope .d))", ".c:has(.d)");
-		} else {
-			assert.step("not : .a:has(:scope) .c and :is(.a :scope .c) returns same elements on scope1");
-			assert.step(".a:has(:scope) .c and :is(.a :scope .c) returns same elements on scope2");
-			assert.step(".c:has(:is(:scope .d)) matches expected elements from scope scope1");
-			assert.step(".c:has(:is(:scope .d)) and :scope .c:has(.d) returns same elements on scope1");
-			assert.step(".c:has(:is(:scope .d)) and .c:has(.d) returns same elements on scope1");
-			assert.step(".c:has(:is(:scope .d)) matches expected elements from scope scope2");
-			assert.step(".c:has(:is(:scope .d)) and :scope .c:has(.d) returns same elements on scope2");
-			assert.step(".c:has(:is(:scope .d)) and .c:has(.d) returns same elements on scope2");
-		}
-
-		assert.verifySteps([
-			":has(:scope) matches expected elements from scope scope1",
-			":has(:scope .c) matches expected elements from scope scope1",
-			":has(.a :scope) matches expected elements from scope scope1",
-			".a:has(:scope) .c matches expected elements from scope scope1",
-			".a:has(:scope) .c matches expected elements from scope scope2",
-			"not : .a:has(:scope) .c and :is(.a :scope .c) returns same elements on scope1",
-			".a:has(:scope) .c and :is(.a :scope .c) returns same elements on scope2",
-			".c:has(:is(:scope .d)) matches expected elements from scope scope1",
-			".c:has(:is(:scope .d)) and :scope .c:has(.d) returns same elements on scope1",
-			".c:has(:is(:scope .d)) and .c:has(.d) returns same elements on scope1",
-			".c:has(:is(:scope .d)) matches expected elements from scope scope2",
-			".c:has(:is(:scope .d)) and :scope .c:has(.d) returns same elements on scope2",
-			".c:has(:is(:scope .d)) and .c:has(.d) returns same elements on scope2"
-		] );
-	});
-
-	QUnit.test(":has matches to uninserted elements", function (assert) {
-		const subject = document.createElement("subject");
-
-		subject.innerHTML = "<child></child>";
-		testMatches(assert, subject, ":has(child)", true);
-		testMatches(assert, subject, ":has(> child)", true);
-
-		subject.innerHTML = "<child><descendant></descendant></child>";
-		testMatches(assert, subject, ":has(descendant)", true);
-		testMatches(assert, subject, ":has(> descendant)", false);
-
-		subject.innerHTML = "<child></child><direct_sibling></direct_sibling><indirect_sibling></indirect_sibling>";
-		testMatches(assert, subject.firstChild, ":has(~ direct_sibling)", true);
-		testMatches(assert, subject.firstChild, ":has(+ direct_sibling)", true);
-		testMatches(assert, subject.firstChild, ":has(~ indirect_sibling)", true);
-		testMatches(assert, subject.firstChild, ":has(+ indirect_sibling)", false);
-
-		testMatches(assert, subject, ":has(*)", true);
-		testMatches(assert, subject, ":has(> *)", true);
-		testMatches(assert, subject, ":has(~ *)", false);
-		testMatches(assert, subject, ":has(+ *)", false);
-
-		assert.verifySteps([
-			":has(child) matches expectedly",
-			":has(> child) matches expectedly",
-			":has(descendant) matches expectedly",
-			":has(> descendant) matches expectedly",
-			":has(~ direct_sibling) matches expectedly",
-			":has(+ direct_sibling) matches expectedly",
-			":has(~ indirect_sibling) matches expectedly",
-			":has(+ indirect_sibling) matches expectedly",
-			":has(*) matches expectedly",
-			":has(> *) matches expectedly",
-			":has(~ *) matches expectedly",
-			":has(+ *) matches expectedly"
 		] );
 	});
 });
